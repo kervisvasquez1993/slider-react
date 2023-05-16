ac2b55b497830b3d6ccedf1e1f1d5513a7d60ad9
kvasquez123 <kvasquez@qantamedia.com>
2023-05-16 11:40:23 -0400

    add base in slider

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Slider } from './Slider'
+
+const App = () => {
+  return (
+    <Slider/>
+  )
+}
+
+export default App
---
+++ src/Slider/Slider.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper";
+
+export const Slider = () => {
+    
+    return (
+        <>
+            <Swiper
+                direction={"horizontal"}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                    position: "left",
+                    type : "bullets"
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
